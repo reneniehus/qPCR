7848d8e3098019a33cc0d48994bccb183d373a70
esthervankleef <esther.vankleef@lshtm.ac.uk>
2016-06-28 17:25:40 +0700

    - Removed samples which were run more than once - Created an patient ID and sample ID variable

--- Manipulate_DATA.r
+++ Manipulate_DATA.r
@@ -1,4 +1,6 @@
 setwd("~/Dropbox/RGNOSIS/Data/SATURN/Yehuda/")
+
+rm(list=ls())
 # load those libraries
 library(dplyr)
 library(car)
@@ -6,11 +8,15 @@ library(tidyr)
 
 # load the data file [this is the one from Pieter], put NA where there is emptiness or undetermined
 DF <- read.csv("./Output16_CTX.csv",na.strings = c("", 'Undetermined'))
+ex_dup <-read.csv("./marked_sample_ex.csv") # Samples which are re-run and should be excluded (on top of the ones Rene has
+                                            # manually exluded at lines 32-77). Selection based on 1) whether marked yellow by
+                                            # agatha. 2) when none of the re-runned samples marked yellow, the first run with
+                                            # valid quantities is taken (i.e. quantity != NA)
 #
 # great habit to look at the first and the last couple of rows
 head(DF)
 tail(DF)
-View(DF)
+#View(DF)
 # to be consistent, change all names in the header to small-case letters
 names(DF) <- tolower(names(DF))
 #
@@ -71,14 +77,39 @@ DF2 <- DF2 %>%
          !(DF2$sample_name == "RM_4332_S2" & DF2$run_name == "20141001_CTX-M_LB.tmp"),
          !(DF2$sample_name == "RM_4586_S1" & DF2$run_name == "20141001_CTX-M2_LB.tmp"))
 # about above: i cannot spot the run "20140922_CTX-M_LB (2)" in the imported data
+DF2 = DF2[order(DF2$sample_name,DF2$type),]
+
+# Samples that have been diluted, multiply quantity by 10 or 100 (as diluted 1:10 or 1:100)
+unique(DF2$sample_name[grep("1:10", DF2$sample_name)])
+DF2$quantity_adj = DF2$quantity
+
+DF2$quantity_adj[grep("1:10", DF2$sample_name)] = DF2$quantity[grep("1:10", DF2$sample_name)]*10
+DF2$quantity_adj[grep("1:100", DF2$sample_name)] = DF2$quantity[grep("1:100", DF2$sample_name)]*100
+
+# Which samples have been re-run
+too_many_samp = (DF2 %>% group_by(sample_name, type) %>% summarise(reps = n()) %>% filter(reps>3))
+#View(DF2[DF2$sample_name%in%too_many_samp$sample_name,])
+
+DF_test = DF2 %>%
+  filter(!(DF2$run_name%in%ex_dup$run & DF2$sample_name%in%ex_dup$sample_name ))
+
+# Check with all unique samples have been included
+length(unique(DF2$sample_name)) ; length(unique(DF_test$sample_name))
+DF2$sample_name[which(!DF2$sample_name%in%(unique(DF_test$sample_name)))] # These samples are all excluded, where actually two shouldn't, as across present samples, all run and sample combinations are all true
+
+# Add first sample runs of these two
+DF2 = rbind(DF_test, DF2[which(!DF2$sample_name%in%(unique(DF_test$sample_name)))[c(1,2,5,6)],])
+length(unique(DF2$sample_name))
+
 
 # Create DF3: remove mean and sd collums and class, make groups based on sample and type, create my own means and standard deviations 
 DF3 <- DF2 %>%
   select(-ct_mean, -ct_sd, -quantity_mean, - quantity_sd, -class) %>%
   group_by(sample_name, type) %>%
-  summarise(ct_mean2=mean(ct,na.rm=TRUE),ct_sd2=sd(ct,na.rm=TRUE),qu_mean2=mean(quantity,na.rm=TRUE),qu_sd2=sd(quantity,na.rm=TRUE),
-            qu_CV=sd(quantity,na.rm=TRUE)/mean(quantity,na.rm=TRUE), reps =n() ) %>%
+  summarise(ct_mean2=mean(ct,na.rm=TRUE),ct_sd2=sd(ct,na.rm=TRUE),qu_mean2=mean(quantity_adj,na.rm=TRUE),qu_sd2=sd(quantity_adj,na.rm=TRUE),
+            qu_CV=sd(quantity_adj,na.rm=TRUE)/mean(quantity_adj,na.rm=TRUE), reps =n() ) %>%
   ungroup()
+
 # Compare DF3 with DF2 in order to see where she calculated different means
 names(DF3)
 names(DF2)
@@ -86,6 +117,8 @@ names(DF2)
 # test <- left_join(DF3, DF2, by=c('sample_name', 'type'))
 test <- left_join(DF2, DF3)
 test$diffctmean <- abs(test$ct_mean2 - test$ct_mean) > 1e-4
+table(test$diffctmean)  # No difference now the extra samples have been removed
+
 # put the 16s and the CTX-M data for one measure into same row, missing data will be NA
 DF16s <- DF3 %>% filter(type=="16S")
 DFCTX <- DF3 %>% filter(type=="CTX-M")
@@ -122,11 +155,19 @@ DF5$qu_ratio[is.na(DF5$ratio_CV)] <- NA # if the CTX has too high varaince and i
 
 tot_excluded = ex1 + ex2 + ex3 + ex4
 
-#
-DF5 <- DF5 %>%
-  filter(!is.na(qu_ratio))
+# Add variable with patient id
+DF5$patient_id = gsub("S[D, 1,2,3,4,5,6,7,8,9,10, =D ]*","",DF5$sample_name)
+
+# Add variable with sample number
+DF6=NULL  
+for(i in unique(DF5$patient_id)){
+  d = DF5[DF5$patient_id == i,]
+  d$s_num = c(1:length(d$sample_name))
+  DF6 = rbind(DF6,d)
+}
+
 # export excel table
-write.csv(DF5, file="./CleanedCTX_M16sRatioErr.csv")
+write.csv(DF6, file="./CleanedCTX_M16sRatioErr.csv")
 
 # What distribution does the within PCR error follow
 # Get to the distribution of the quantitiy control sample
