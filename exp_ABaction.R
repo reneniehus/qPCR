bef68c4b73d3e994854875a70d74782b58a92146
Rene <rene.niehus@gmx.de>
2016-08-09 13:16:00 +0700

    Worked on regression

--- exp_ABaction.R
+++ exp_ABaction.R
@@ -14,7 +14,7 @@ library(reshape)
 setwd("~/Dropbox/LOMHWRU_MORU/SATURN_ESBL/_R/R_git/qPCR/")
 
 ### My functions
-# funcion that compares neighbouring elements of a data vector
+# function that compares neighbouring elements of a data vector
 Comp <- function(data)
 {
   output <- vector()
@@ -36,29 +36,19 @@ Comp <- function(data)
 SDATA <- read.csv ("./Cleaned_data/linked_qPCR_clin_abx.csv",
                    sep= ",", colClasses=c("character")) # Linked Data
 
-# turn into numerical 
+### Mutations  
+# turn into numerical
 SDATA <- SDATA %>% 
-  plyr::mutate(
+  dplyr::mutate(
          ratio_SD = as.numeric(ratio_SD),
          s_num = as.numeric(s_num),
          qu_ratio = as.numeric(qu_ratio),
-         esbl_act = as.numeric(esbl_act),
-         broad_spec = as.numeric(broad_spec),
-         Piperacillin.tazobactam = as.numeric(Piperacillin.tazobactam),
-         Vancomycin = as.numeric(Vancomycin),
-         isoniazid = as.numeric(isoniazid),
-         Levofloxacin = as.numeric(Levofloxacin),
-         Meropenem = as.numeric(Meropenem),
-         pyrazinamide = as.numeric(pyrazinamide),
-         Rifampicin = as.numeric(Rifampicin),
-         Ampicillin = as.numeric(Ampicillin),
-         Oxacillin = as.numeric(Oxacillin),
-         Ceftriaxone = as.numeric(Ceftriaxone),
          ESBL_Ecoli = as.numeric(ESBL_Ecoli),
          ESBL_PPM = as.numeric(ESBL_PPM),
          ESBL_KESC = as.numeric(ESBL_KESC)
   )
-
+# then also mutate the range of Antibiotics!
+SDATA <- SDATA %>% dplyr::mutate_each(funs(as.numeric),esbl_act:Cefalexin)
 # turn into dates
 SDATA <- SDATA %>%
   plyr::mutate(RectalDate = as.Date(as.character(SDATA$RectalDate), format="%Y-%m-%d"))
@@ -73,38 +63,31 @@ names(SDATA) # gives a nice list of a the variables
 str(SDATA) # gives a summary like in the global environemnt
 levels(SDATA$patient_id) # this is only for categorical data
 summary(SDATA$qu_ratio) # gives a summery of a coloumn
-# the median is 0.0077: majority of measures are close to zero. I expect that this ratio is mostly very small. 
+# the median is 0.0077: majority of measures are close to zero.
+# I expect that this ratio is mostly very small. 
 # how accurate are small values of the ratio. Look at the error
 summary(SDATA$ratio_SD) # gives a summery of a coloumn
 # the SD of the ratio is around 1% of 
 mean.quratio = mean(SDATA$qu_ratio, na.rm = T) # calculate the mean
 max.quratio = max(SDATA$qu_ratio, na.rm = T) # calculate the mean
-# make a histogram and density plot
-png('./Figures/histogram and kernel density plot.png')
-hist(SDATA$qu_ratio, breaks = 5, freq = F, xlab = 'CTXm-16s ratio', xlim = c(0,20), ylim = c(0, 1), ylab = 'Probability', main = 'Histogram of CTXm abundance with Kernel Density Plot')
-lines(density(SDATA$qu_ratio, na.rm = T, from = 0, to = max.quratio))
-dev.off()
 
 ########
 # small table version of SDATA
 #names(SDATA)[90:126] # those are all the antibiotics in action
 DF1 <- SDATA %>%
-  dplyr::select(num,qu_ratio,Tdiff,RectalDate,esbl_act,broad_spec,
-                Piperacillin.tazobactam,Vancomycin,isoniazid,Levofloxacin,
-                Meropenem,pyrazinamide,Rifampicin,Ampicillin,Oxacillin,
-                Ceftriaxone,Ceftazidime)
+  dplyr::select(num,qu_ratio,Tdiff,RectalDate,esbl_act:Cefalexin)
 
 # replace all NA by 0
 DF1[is.na(DF1)] <- 0
 
-# remove the qu_ratio over 100
+# remove 2 outliers [maybe outliers, why are they outliers?]
 DF1 <- DF1[-which(max(DF1$qu_ratio) == DF1$qu_ratio),]
 DF1 <- DF1[-which(max(DF1$qu_ratio) == DF1$qu_ratio),]
 
 # add the time since last measurement as predictor
 # add a column with previous date
 next.date <- c(DF1$RectalDate[2:(length(DF1$RectalDate))] , as.Date("2011-1-1"))
-DF1$TdiffToNext <- next.date - DF1$RectalDate
+DF1$DaysToNextQuRatio <- next.date - DF1$RectalDate
 
 # the next qu_ratio is what i want to predict
 DF1$NextQuRatio = c(DF1$qu_ratio[2 : (nrow(DF1))], 0 )
@@ -113,19 +96,47 @@ DF1$NextQuRatio = c(DF1$qu_ratio[2 : (nrow(DF1))], 0 )
 first.meas = which(DF1$num == 0)
 first.meas <- first.meas[2 : length(first.meas)] - 1
 
-# Filter away first measurements
-DF2 <- DF1[-first.meas,]
+# Filter away first measurements and de-select Tdiff
+DF2 <- DF1[-first.meas,] %>% dplyr::select(-Tdiff)
 
-# Make new DF for regression
-DF3 <- DF2 %>% dplyr::select(num,qu_ratio,esbl_act,broad_spec,
-                             Piperacillin.tazobactam,Vancomycin,isoniazid,Levofloxacin,
-                             Meropenem,pyrazinamide,Rifampicin,Ampicillin,Oxacillin,
-                             Ceftriaxone,Ceftazidime,
-                             TdiffToNext,NextQuRatio)
+### Regression with interaction [important because most points are 
+# small ratio - to small ratio, forces coefficient of binary to zero]
 
-### Now perform different regressions
+# fit.3: broad_spec, qu_ratio + interaction
+DF_R1 <-  DF2 %>% dplyr::select(num,qu_ratio,broad_spec,NextQuRatio)
+DF_R1 <- mutate(
+  DF_R1,qu_ratio = (qu_ratio - mean(qu_ratio))/(2*(sd(qu_ratio))),
+  broad_spec = 
+)
+
+fit.3 <- lm(DF_R1$NextQuRatio ~ DF_R1$broad_spec + DF_R1$qu_ratio + DF_R1$broad_spec:DF_R1$qu_ratio)
+fit.3
+# graphical display
+colors <- ifelse(DF_R1$broad_spec==1, "black", "gray")
+plot(DF_R1$qu_ratio, DF_R1$NextQuRatio, xlab="Previous QuRatio", ylab="Next Qu",
+     col=colors, pch=20)
+curve(cbind(1, 1, x, 1*x) %*% coef(fit.3), add=TRUE, col="black")
+curve(cbind(1, 0,x, 0*x) %*% coef(fit.3), add=TRUE, col="gray")
+
+# fit.4: esbl_act, qu_ratio + interaction
+DF_R4 <-  DF2 %>% dplyr::select(num,qu_ratio,esbl_act,NextQuRatio) 
+DF_R4$esbl_act[DF_R4$esbl_act == 2] <- 1 # make maybe = yes
+fit.4 <- lm(DF_R4$NextQuRatio ~ DF_R4$esbl_act + DF_R4$qu_ratio + DF_R4$esbl_act:DF_R4$qu_ratio)
+fit.4
+# graphical display
+colors <- ifelse(DF_R4$esbl_act==1, "black", "gray")
+plot(DF_R4$qu_ratio, DF_R4$NextQuRatio, xlab="Previous QuRatio", ylab="Next Qu",
+     col=colors, pch=20)
+curve(cbind(1, 1, x, 1*x) %*% coef(fit.4), add=TRUE, col="black")
+curve(cbind(1, 0,x, 0*x) %*% coef(fit.4), add=TRUE, col="gray")
+
+# Output: NextQuRatio -- Predicting Features: qu_ratio, esbl_act
+
+
+
+### Regressions without interaction term
 # Output: NextQuRatio -- Predicting Features: qu_ratio, esbl_act, broad_spec
-DF_R1 <-  DF3 %>% dplyr::select(num,qu_ratio,broad_spec,NextQuRatio)
+DF_R1 <-  DF2 %>% dplyr::select(num,qu_ratio,broad_spec,NextQuRatio)
 
 # Fit the NextQuRatio using the previous and broad_spec
 fit.1 <- lm(DF_R1$NextQuRatio ~ DF_R1$broad_spec + DF_R1$qu_ratio)
@@ -149,20 +160,3 @@ plot(DF_R2$qu_ratio, DF_R2$NextQuRatio, xlab="Previous QuRatio", ylab="Next Qu",
      col=colors, pch=20)
 curve(cbind(1, 1, x) %*% coef(fit.2), add=TRUE, col="black")
 curve(cbind(1, 0,x) %*% coef(fit.2), add=TRUE, col="gray")
-
-# allow for interactions: important because we expect 2nd variable to only be effective at non-zero esbl!
-fit.3 <- lm(DF_R1$NextQuRatio ~ DF_R1$broad_spec + DF_R1$qu_ratio + DF_R1$broad_spec:DF_R1$qu_ratio)
-fit.3
-# graphical display
-colors <- ifelse(DF_R1$broad_spec==1, "black", "gray")
-plot(DF_R1$qu_ratio, DF_R1$NextQuRatio, xlab="Previous QuRatio", ylab="Next Qu",
-     col=colors, pch=20)
-curve(cbind(1, 1, x, 1*x) %*% coef(fit.3), add=TRUE, col="black")
-curve(cbind(1, 0,x, 0*x) %*% coef(fit.3), add=TRUE, col="gray")
-
-# Output: NextQuRatio -- Predicting Features: qu_ratio, esbl_act, broad_spec, TdiffToNext
-
-### play around with data cleaning
-SDATA
-
-
